57902fc53d5d3fbb80ae1d6e56987a81fe5000d9
kowligi1998@gmail.com <kowligi1998@gmail.com>
2024-03-28 22:49:26 -0500

    updates

--- PhoneBook_Starter_Java_SpringBoot/src/main/java/com/cse5382/assignment/Service/PhoneBookService.java
+++ PhoneBook_Starter_Java_SpringBoot/src/main/java/com/cse5382/assignment/Service/PhoneBookService.java
@@ -4,6 +4,9 @@ import com.cse5382.assignment.Model.PhoneBookEntry;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface PhoneBookService {
     public List<PhoneBookEntry> list();
     public void add(PhoneBookEntry phoneBookEntry);
